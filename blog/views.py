ec87e3d5988fdf55308ea848f78e2c095cf73d8e
Eliot00 <hack00mind@gmail.com>
2018-04-12 17:50:15 +0800

    添加markdown支持

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
+import markdown
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.http import Http404
 
-from blog.models import Article
+from .models import Article
 # Create your views here.
 def index(request):
     post_list = Article.objects.all()
@@ -12,6 +13,12 @@ def index(request):
 def detail(request, id):
     try:
         post = Article.objects.get(id=str(id))
+        post.body = markdown.markdown(post.body,
+                extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+                'markdown.extensions.toc',
+                ])
     except Article.DoesNotExist:
         raise Http404
     return render(request, 'detail.html', {'post':post})
